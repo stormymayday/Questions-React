3494c89030ce5ce33009c58c16f310ac1459cce9
stormymayday <stormymayday7@gmail.com>
2023-12-28 23:26:05 -0800

    reset

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,4 @@
-import { useState } from 'react';
-import Questions from './components/Questions.jsx';
-import data from './data.js';
-
-// Original:
-// const App = () => {
-
-//   const [questions, setQuestions] = useState(data);
-
-//   return (
-//     <main>
-//       <Questions questions={questions} />
-//     </main>
-
-//   );
-
-// };
-
-// Alternative:
 const App = () => {
-
-  const [questions, setQuestions] = useState(data);
-  const [activeId, setActiveId] = useState(null);
-
-  const toggleQuestion = (id) => {
-
-    // If question 'id' is equal to 'activeId', set it to null.
-    // Otherwise, set it to 'id'
-    const newActiveId = id === activeId ? null : id;
-
-    setActiveId(newActiveId);
-
-  };
-
-  return (
-    <main>
-      <Questions questions={questions} activeId={activeId} toggleQuestion={toggleQuestion} />
-    </main>
-
-  );
-
+    return <h2>Accordion Starter</h2>;
 };
-
 export default App;
